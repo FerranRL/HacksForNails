1c0f19309b39c7c93d33a143151f7bddc441f860
FerranRL <43423120+FerranRL@users.noreply.github.com>
2024-09-12 10:17:19 +0200

    Changes to AppointmentView

--- HacksForNails/Components/AppointmentView.swift
+++ HacksForNails/Components/AppointmentView.swift
@@ -9,43 +9,61 @@
 import SwiftUI
 
 struct AppointmentView: View {
-            var name: String
-            var service: String
-            var time: String
-            var price: String
-            var imageName: String
-
-            var body: some View {
-                HStack {
-                    Image(imageName)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 50, height: 50)
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text(name)
-                            .font(.headline)
-                            .fontWeight(.bold)
-                        
-                        Text(service)
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                        
-                        Text(time)
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                    }
-                    Spacer()
-                    
-                    Text(price)
-                        .font(.headline)
-                        .foregroundColor(.black)
-                }
-                .padding(.vertical, 10)
-                .padding(.horizontal)
-                .background(Color.white)
-                .cornerRadius(10)
-                .shadow(radius: 2)
+    var name: String
+    var service: String
+    var time: String
+    var price: String
+    var imageName: String
+    var showMore: Bool? = true
+    
+    var body: some View {
+        HStack {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 79, height: 79)
+                .background(Color.black.opacity(0.1))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(name)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                
+                Text(service)
+                    .font(.caption)
+                    .foregroundColor(.black)
+                
+                Spacer()
+                
+                Text(time)
+                    .font(.callout)
+                    .foregroundColor(.black)
             }
+            Spacer()
+            
+            VStack {
+                if showMore == true {
+                    Image(systemName: "ellipsis")
+                        .foregroundStyle(.gray)
+                        .padding(.top, 8)
+                }
+
+                Spacer()
+                
+                Text(price)
+                    .font(.headline)
+                    .foregroundColor(.black)
+            }
+            
         }
+        .frame(height: 79)
+        .background(Color.white)
+        .padding(.bottom, 10)
+    }
+}
+
+#Preview {
+    StylistView()
+}
--- HacksForNails/View/StylistView.swift
+++ HacksForNails/View/StylistView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct StylistView: View {
     @EnvironmentObject var loginModel: LoginViewModel
+    @State var currentUser = LoginViewModel.shared.currentUser
     @State private var showMenu: Bool = false
+    @State private var selectedDate = Date()
+    @State private var showingDatePicker = false
 
     var body: some View {
         // Integración del AnimatedSideBar
@@ -44,10 +47,11 @@ struct StylistView: View {
                             }
                             .padding(.top, 2)
 
-                            Text("Stylist View")
+                            Text("¡Hola \(currentUser?.firstName ?? "Invitado")!")
                                 .font(.title)
                                 .fontWeight(.bold)
                                 .foregroundColor(.white)
+                            
                             Spacer()
                             Image(systemName: "bell")
                                 .foregroundColor(.white)
@@ -64,12 +68,65 @@ struct StylistView: View {
                         }
                         .padding(.top, 40)
                         .padding(.horizontal)
-                        .background(Color.black.opacity(0.7))
-
+                        //.background(Color.black.opacity(0.7))
+                        
+                        HStack {
+                            Text("Esta es tu agenda de hoy")
+                                .font(.title2)
+                                .foregroundStyle(.white)
+                                .padding(.horizontal)
+                            Spacer()
+                        }
+                        
+                        
+                        
                         // Resto del contenido del dashboard
                         ScrollView {
-                            UpcomingAppointmentsView()
+                            VStack(spacing: 10) {
+                                
+                                HStack {
+                                    Button(action: {
+                                        showingDatePicker.toggle()
+                                    }) {
+                                        HStack {
+                                            Spacer()
+                                            Text(formattedDate(selectedDate))
+                                                .font(.headline)
+                                                .foregroundColor(.black)
+                                                
+                                            
+                                        }
+                                    }
+                                    .popover(isPresented: $showingDatePicker) {
+                                        VStack {
+                                            DatePicker(
+                                                "Selecciona una fecha",
+                                                selection: $selectedDate,
+                                                displayedComponents: .date
+                                            )
+                                            .datePickerStyle(.graphical)
+                                            .padding()
+                                        }
+                                    }
+                                    
+                                }
+                                .padding(.bottom, 10)
+                                
+                                Divider()
+                                    .padding(.bottom, 10)
+                                // Aquí agregamos las citas
+                                AppointmentView(name: "Mireia Carrera", service: "Semipermanente Manos Básica Refuerzo Gel en Uñas", time: "10:00 - 11:00", price: "22€", imageName: "person1")
+                                AppointmentView(name: "Joan Parera", service: "Cutículas y vitaminas", time: "11:00 - 11:20", price: "10€", imageName: "person2")
+                                AppointmentView(name: "Rosa Vila", service: "Nail Art", time: "11:20 - 13:20", price: "55€", imageName: "person3")
+                            }
+                            
+                            
                         }
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .padding(.horizontal)
+                        .padding(.bottom, 40)
                     }
                 }
                 .background(Color.black)
@@ -81,6 +138,13 @@ struct StylistView: View {
                 .foregroundStyle(.gray)
         }
     }
+    
+    private func formattedDate(_ date: Date) -> String {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "es_ES") // Establece el idioma español
+            formatter.dateFormat = "EEEE, d 'de'  MMMM 'de' yyyy" // Formato personalizado
+            return formatter.string(from: date)
+        }
 
     @ViewBuilder
     func SideBarMenuView(_ safeArea: UIEdgeInsets) -> some View {
@@ -98,13 +162,10 @@ struct StylistView: View {
                 .background(.white)
                 .padding(.vertical, 10)
 
-            SideBarButton(.vista)
             SideBarButton(.citas)
-            SideBarButton(.clientes)
             SideBarButton(.estilistas)
             SideBarButton(.mensajes)
             SideBarButton(.reseñas)
-            SideBarButton(.contabilidad)
             SideBarButton(.ajustes)
             
 
@@ -122,7 +183,7 @@ struct StylistView: View {
     }
 
     @ViewBuilder
-    func SideBarButton(_ tab: DashboardView.dash, onTap: @escaping () -> () = { }) -> some View {
+    func SideBarButton(_ tab: StylistView.dash, onTap: @escaping () -> () = { }) -> some View {
         Button(action: {
             if tab == .ajustes {
                 showMenu = false
@@ -146,25 +207,20 @@ struct StylistView: View {
     }
     
     enum dash: String, CaseIterable {
-        case vista = "house.fill"
+        
         case citas = "calendar"
-        case clientes = "person.fill"
         case estilistas = "person.crop.circle.fill"
         case mensajes = "mail.stack.fill"
         case reseñas = "star.leadinghalf.filled"
-        case contabilidad = "creditcard.fill"
         case ajustes = "gear"
         case logout = "rectangle.portrait.and.arrow.right"
         
         var title: String {
             switch self {
-            case .vista: return "Vista general"
             case .citas: return "Citas"
-            case .clientes: return "Clientes"
-            case .estilistas: return "Estilistas"
+            case .estilistas: return "Mi Perfil"
             case .mensajes: return "Mensajes"
             case .reseñas: return "Reseñas"
-            case .contabilidad: return "Contabilidad"
             case .ajustes: return "Ajustes"
             case .logout: return "Cerrar sesión"
             }
